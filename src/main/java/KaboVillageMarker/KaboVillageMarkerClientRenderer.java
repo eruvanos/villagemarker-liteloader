e174aa4de5410039c1139a385743f92503d324b9
eruvanos <eruvanos@ewetel.net>
2017-03-12 11:41:35 +0100

    Fix ConcurrentModificationException for 1.11.2

--- src/main/java/KaboVillageMarker/KaboVillageMarkerClientRenderer.java
+++ src/main/java/KaboVillageMarker/KaboVillageMarkerClientRenderer.java
@@ -55,96 +55,103 @@ public class KaboVillageMarkerClientRenderer {
     @SuppressWarnings("unchecked")
     private static void renderSpheresAndLines(Tessellator tess, Entity renderEntity, float partialTicks) {
 
-        int c = 0;
+        final int[] c = {0};
         VertexBuffer vb = tess.getBuffer();
 
-        for (Village village : (List<Village>) mc.getIntegratedServer().worldServerForDimension(mc.thePlayer.dimension).getVillageCollection().getVillageList()) {
-            int vRad = village.getVillageRadius();
-            BlockPos vCen = village.getCenter();
-            List<VillageDoorInfo> vDoors = new ArrayList<VillageDoorInfo>();
-            vDoors.addAll(village.getVillageDoorInfoList());
+        List<Village> villageList = mc.getIntegratedServer()
+                .worldServerForDimension(mc.thePlayer.dimension)
+                .getVillageCollection()
+                .getVillageList();
+
+        //Avoid ConcurrentModificationException
+        new ArrayList<>(villageList).forEach(
+                village -> {
+                    int vRad = village.getVillageRadius();
+                    BlockPos vCen = village.getCenter();
+                    List<VillageDoorInfo> vDoors = new ArrayList<>();
+                    vDoors.addAll(village.getVillageDoorInfoList());
 
 
-            if (LiteModKaboVillageMarker.fullbright) {
-                RenderHelper.disableStandardItemLighting();
-                OpenGlHelper.setLightmapTextureCoords(OpenGlHelper.lightmapTexUnit, 240, 240);
-            }
-
-            GlStateManager.enableBlend();
-            GlStateManager.disableTexture2D();
-            GlStateManager.blendFunc(GL11.GL_ONE, GL11.GL_CONSTANT_COLOR);
-            setColor(c % 6);
-            ++c;
-            GL11.glPointSize((float) (KaboModSettings.instance.dotSize + 1));
-            GlStateManager.glLineWidth(3.0F);
-            GL11.glEnable(GL11.GL_POINT_SMOOTH);
-            GlStateManager.depthMask(false);
-            glPushMatrix();
-
-
-            vb.setTranslation(-getPlayerXGuess(renderEntity, partialTicks),
-                              -getPlayerYGuess(renderEntity, partialTicks),
-                              -getPlayerZGuess(renderEntity, partialTicks));
-            // tess.getWorldRenderer().setTranslation(-xOff, -yOff, -zOff);
-
-            if (KaboModSettings.instance.drawSphere) {
-                float villageDoors = KaboModSettings.instance.sphereDensity;
-                int i$ = 24 + (int) (villageDoors * 72.0F);
-                double[] dObj = new double[i$ * (i$ / 2 + 1)];
-                double[] doorInfo = new double[i$ * (i$ / 2 + 1)];
-                double[] ys = new double[i$ * (i$ / 2 + 1)];
-
-                for (double t = 0.0D; t < 6.283185307179586D; t += 6.283185307179586 / (double) i$) {
-                    for (double theta = 0.0D; theta < Math.PI; theta += Math.PI / (double) (i$ / 2)) {
-                        double dx = (double) vRad * Math.sin(t) * Math.cos(theta);
-                        double dz = (double) vRad * Math.sin(t) * Math.sin(theta);
-                        double dy = (double) vRad * Math.cos(t);
-                        int index = (int) (t / (6.283185307179586D / (double) i$) + (double) i$ * theta / (3.141592653589793D / (double) (i$ / 2)));
-                        dObj[index] = (double) vCen.getX() + dx;
-                        doorInfo[index] = (double) vCen.getZ() + dz;
-                        ys[index] = (double) vCen.getY() + dy;
+                    if (LiteModKaboVillageMarker.fullbright) {
+                        RenderHelper.disableStandardItemLighting();
+                        OpenGlHelper.setLightmapTextureCoords(OpenGlHelper.lightmapTexUnit, 240, 240);
                     }
-                }
+
+                    GlStateManager.enableBlend();
+                    GlStateManager.disableTexture2D();
+                    GlStateManager.blendFunc(GL11.GL_ONE, GL11.GL_CONSTANT_COLOR);
+                    setColor(c[0] % 6);
+                    ++c[0];
+                    GL11.glPointSize((float) (KaboModSettings.instance.dotSize + 1));
+                    GlStateManager.glLineWidth(3.0F);
+                    GL11.glEnable(GL11.GL_POINT_SMOOTH);
+                    GlStateManager.depthMask(false);
+                    glPushMatrix();
 
 
-                // tess.getWorldRenderer().startDrawing(GL11.GL_POINTS);
-                vb.begin(GL11.GL_POINTS, DefaultVertexFormats.POSITION);
+                    vb.setTranslation(-getPlayerXGuess(renderEntity, partialTicks),
+                            -getPlayerYGuess(renderEntity, partialTicks),
+                            -getPlayerZGuess(renderEntity, partialTicks));
+                    // tess.getWorldRenderer().setTranslation(-xOff, -yOff, -zOff);
 
-                for (int var34 = 0; var34 < i$ * (i$ / 2 + 1); ++var34) {
-                    vb.pos(dObj[var34], ys[var34], doorInfo[var34]).endVertex();
-                }
+                    if (KaboModSettings.instance.drawSphere) {
+                        float villageDoors = KaboModSettings.instance.sphereDensity;
+                        int i$ = 24 + (int) (villageDoors * 72.0F);
+                        double[] dObj = new double[i$ * (i$ / 2 + 1)];
+                        double[] doorInfo = new double[i$ * (i$ / 2 + 1)];
+                        double[] ys = new double[i$ * (i$ / 2 + 1)];
 
-                tess.draw();
-            }
+                        for (double t = 0.0D; t < 6.283185307179586D; t += 6.283185307179586 / (double) i$) {
+                            for (double theta = 0.0D; theta < Math.PI; theta += Math.PI / (double) (i$ / 2)) {
+                                double dx = (double) vRad * Math.sin(t) * Math.cos(theta);
+                                double dz = (double) vRad * Math.sin(t) * Math.sin(theta);
+                                double dy = (double) vRad * Math.cos(t);
+                                int index = (int) (t / (6.283185307179586D / (double) i$) + (double) i$ * theta / (3.141592653589793D / (double) (i$ / 2)));
+                                dObj[index] = (double) vCen.getX() + dx;
+                                doorInfo[index] = (double) vCen.getZ() + dz;
+                                ys[index] = (double) vCen.getY() + dy;
+                            }
+                        }
 
-            // tess.getWorldRenderer().startDrawing(GL11.GL_LINES);
-            vb.begin(GL11.GL_LINES, DefaultVertexFormats.POSITION);
 
-            for (VillageDoorInfo vdi : vDoors) {
-                BlockPos var33 = vdi.getDoorBlockPos();
-                vb.pos((double) var33.getX(), (double) var33.getY(), (double) var33.getZ()).endVertex();
-                vb.pos((double) vCen.getX(),  (double) vCen.getY(),  (double) vCen.getZ() ).endVertex();
-            }
+                        // tess.getWorldRenderer().startDrawing(GL11.GL_POINTS);
+                        vb.begin(GL11.GL_POINTS, DefaultVertexFormats.POSITION);
 
-            tess.draw();
-            if (KaboModSettings.instance.drawGolemArea) {
-                renderGolemBox(tess, vCen, c);
-            }
+                        for (int var34 = 0; var34 < i$ * (i$ / 2 + 1); ++var34) {
+                            vb.pos(dObj[var34], ys[var34], doorInfo[var34]).endVertex();
+                        }
 
-            glPopMatrix();
+                        tess.draw();
+                    }
 
-            if (LiteModKaboVillageMarker.fullbright) {
-                // GL11.glEnable(GL11.GL_LIGHTING);
-                RenderHelper.enableStandardItemLighting();
-            }
+                    // tess.getWorldRenderer().startDrawing(GL11.GL_LINES);
+                    vb.begin(GL11.GL_LINES, DefaultVertexFormats.POSITION);
 
-            GlStateManager.disableBlend();
-            GlStateManager.enableTexture2D();
-            GlStateManager.depthMask(true);
-            // tess.getWorldRenderer().setTranslation(0.0D, 0.0D, 0.0D);
-            vb.setTranslation(0.0D, 0.0D, 0.0D);
+                    for (VillageDoorInfo vdi : vDoors) {
+                        BlockPos var33 = vdi.getDoorBlockPos();
+                        vb.pos((double) var33.getX(), (double) var33.getY(), (double) var33.getZ()).endVertex();
+                        vb.pos((double) vCen.getX(), (double) vCen.getY(), (double) vCen.getZ()).endVertex();
+                    }
 
-        }
+                    tess.draw();
+                    if (KaboModSettings.instance.drawGolemArea) {
+                        renderGolemBox(tess, vCen, c[0]);
+                    }
+
+                    glPopMatrix();
+
+                    if (LiteModKaboVillageMarker.fullbright) {
+                        // GL11.glEnable(GL11.GL_LIGHTING);
+                        RenderHelper.enableStandardItemLighting();
+                    }
+
+                    GlStateManager.disableBlend();
+                    GlStateManager.enableTexture2D();
+                    GlStateManager.depthMask(true);
+                    // tess.getWorldRenderer().setTranslation(0.0D, 0.0D, 0.0D);
+                    vb.setTranslation(0.0D, 0.0D, 0.0D);
+
+                });
 
     }
 
@@ -170,8 +177,8 @@ public class KaboVillageMarkerClientRenderer {
 
             glPushMatrix();
             vb.setTranslation(-getPlayerXGuess(renderEntity, partialTicks),
-                              -getPlayerYGuess(renderEntity, partialTicks),
-                              -getPlayerZGuess(renderEntity, partialTicks));
+                    -getPlayerYGuess(renderEntity, partialTicks),
+                    -getPlayerZGuess(renderEntity, partialTicks));
 
             GlStateManager.blendFunc(GL11.GL_ONE, GL11.GL_CONSTANT_COLOR);
             setColor(c % 6);
@@ -215,7 +222,7 @@ public class KaboVillageMarkerClientRenderer {
 
             while (var31.hasNext()) {
                 KaboVillageMarkerClient.KaboVillageMarkerVillageDoorPosition var30 = (KaboVillageMarkerClient.KaboVillageMarkerVillageDoorPosition) var31.next();
-                vb.pos((double) var30.x,     (double) var30.y,     (double) var30.z    ).endVertex();
+                vb.pos((double) var30.x, (double) var30.y, (double) var30.z).endVertex();
                 vb.pos((double) vCen.getX(), (double) vCen.getY(), (double) vCen.getZ()).endVertex();
             }
 
@@ -313,8 +320,8 @@ public class KaboVillageMarkerClientRenderer {
                 if (xi == -8 || xi == 8 || yi == -3 || yi == 3) {
                     vb.pos((double) (vCen.getX() + xi), (double) (vCen.getY() + yi), (double) (vCen.getZ() - 8)).endVertex();
                     vb.pos((double) (vCen.getX() + xi), (double) (vCen.getY() + yi), (double) (vCen.getZ() + 8)).endVertex();
-                    vb.pos((double) (vCen.getX() - 8),  (double) (vCen.getY() + yi), (double) (vCen.getZ() + xi)).endVertex();
-                    vb.pos((double) (vCen.getX() + 8),  (double) (vCen.getY() + yi), (double) (vCen.getZ() + xi)).endVertex();
+                    vb.pos((double) (vCen.getX() - 8), (double) (vCen.getY() + yi), (double) (vCen.getZ() + xi)).endVertex();
+                    vb.pos((double) (vCen.getX() + 8), (double) (vCen.getY() + yi), (double) (vCen.getZ() + xi)).endVertex();
                 }
             }
 
